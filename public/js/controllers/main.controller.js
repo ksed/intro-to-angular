aaedff0f879f654a1999eb5f972310dd8f9c3cdc
ksed <hydrogeo.coder@gmail.com>
2016-10-24 20:37:23 -0400

    handled duplication issues in list and cleared the input after creation of new todo

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -5,13 +5,12 @@
   MainController.$inject = ['$scope', 'TodoService']; // What tools the MainController function needs.
 
   function MainController($scope, TodoService) { // scope is our bridge to the DOM;
-    console.log(TodoService.get());
-    TodoService.create('make more tools');
-    console.log(TodoService.get());
-    TodoService.update(0, 'buy some ramen');
-    console.log(TodoService.get());
-    TodoService.delete(2);
-    console.log(TodoService.get());
-    console.log(TodoService.get());
+    $scope.todos = TodoService.get();
+    $scope.createTodo = createTodo;
+
+    function createTodo(newTodo) {
+      TodoService.create(newTodo);
+      $scope.newTodo = '';
+    }
   }
 }());
