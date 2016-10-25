88888caf5f9b5a042f767284c4f23bbb8057a25e
ksed <hydrogeo.coder@gmail.com>
2016-10-24 20:49:55 -0400

    added a remove function to the list

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -7,10 +7,15 @@
   function MainController($scope, TodoService) { // scope is our bridge to the DOM;
     $scope.todos = TodoService.get();
     $scope.createTodo = createTodo;
+    $scope.deleteTodo = deleteTodo;
 
     function createTodo(newTodo) {
       TodoService.create(newTodo);
       $scope.newTodo = '';
+    }
+
+    function deleteTodo(index) {
+      TodoService.delete(index);
     }
   }
 }());
