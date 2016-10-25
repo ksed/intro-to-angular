28c85df2a3c621fc7255bada31c31bc20ae68c94
ksed <hydrogeo.coder@gmail.com>
2016-10-25 18:49:10 -0400

    added ability to edit and save edits per browser session

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -8,14 +8,22 @@
     $scope.todos = TodoService.get();
     $scope.createTodo = createTodo;
     $scope.deleteTodo = deleteTodo;
+    $scope.editTodo = editTodo;
+    $scope.saveTodo = saveTodo;
 
     function createTodo(newTodo) {
       TodoService.create(newTodo);
       $scope.newTodo = '';
     }
-
     function deleteTodo(index) {
       TodoService.delete(index);
+    }
+    function editTodo(todo) {
+      todo.isBeingEdited = true;
+    }
+    function saveTodo(index, todo) {
+      TodoService.update(index, todo.desc);
+      todo.isBeingEdited = false;
     }
   }
 }());
